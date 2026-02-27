2ac249988e5be7a4b7643cab5a1ec97cbe441420
Salim Kayal <salim.kayal@gmail.com>
2026-02-27 22:34:47 +0100

    feat: parametrize with env variables

---
+++ src/_data/site.js
@@ -0,0 +1,16 @@
+export default {
+    "name": "Oser Soi",
+    "tagline": "Parce que chaque chemin est unique",
+    "subtitle": "Hypnose Ericksonienne & PNL",
+    "description": "Accompagnement bienveillant vers le mieux-être avec l'hypnose Ericksonienne et la PNL.",
+    "therapist": process.env.THERAPIST,
+    "phone": process.env.PHONE,
+    "email": process.env.EMAIL,
+    "address": process.env.ADDRESS,
+    "horaires": "Un mercredi sur deux, de 8h00 à 18h30",
+    "web3forms_key": process.env.WEB3FORMS_KEY,
+    "url": process.env.URL,
+    "calcom": process.env.CALCOM,
+    "host": "GitHub",
+};
+
